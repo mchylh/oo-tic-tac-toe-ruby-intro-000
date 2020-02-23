7bdde1bd35672d1067208d5920822f613ba47b5e
Michaylah Mam <mchylh@gmail.com>
2020-02-23 01:26:01 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -5,167 +5,116 @@ class TicTacToe
     @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   end
 
-  #WIN_COMBINATIONS is a constant but it’s the parent array to our the children array.
-  #parent_array => WIN_COMBINATIONS = [ children_arrays ]
-  #children_arrays => [0,1,2],[3,4,5],[6,7,8]...[6,4,2]
   WIN_COMBINATIONS = [
-  [0,1,2], # top_row
-  [3,4,5], # middle_row
-  [6,7,8], # bottom_row
-  [0,3,6], # left_column
-  [1,4,7], # center_column
-  [2,5,8], # right_column
-  [0,4,8], # left_diagonal
-  [6,4,2] # right_diagonal
-  ]
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [6, 4, 2],
+  [0, 4, 8]
+]
 
-  #build structure of the board
-  puts board = ["  ","  ","  ","  ","  ","  ","  ","  ","  "]
-
-  #display_board method
   def display_board
-     puts row = ["   " "|" "   " "|" "   "]
-     puts separator = "-----------"
-     puts row
-     puts separator
-     puts row
-   end
-
-  #board and position
-  def display_board(board)
-     puts " #{board[0]} | #{board[1]} | #{board[2]} "
-     puts "-----------"
-     puts " #{board[3]} | #{board[4]} | #{board[5]} "
-     puts "-----------"
-     puts " #{board[6]} | #{board[7]} | #{board[8]} "
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts " ----------- "
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts " ----------- "
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
 
-  #change player input in to integer
-  def input_to_index(user_input)
-    user_input.to_i - 1
+  def input_to_index(input)
+    input.to_i - 1
   end
 
-  def move(board, index, marker)
-   board[index] = marker
+  def move(position, token='X')
+    @board[position] = token
   end
 
-  def position_taken? (board, index)
-    if board[index] == "" || board[index] == " " || board[index] == nil
-      return false
+  def position_taken?(input)
+    @board[input] == "X" || @board[input] == "O"
+  end
+
+  def valid_move?(input)
+    input.between?(0, 8) && !position_taken?(input)
+  end
+
+
+  def turn
+    puts "Choose a spot between 1-9"
+    spot = gets.strip
+    spot = input_to_index(spot)
+    if valid_move?(spot)
+      move(spot, current_player)
     else
-      return true
+      turn
     end
+    display_board
   end
 
-  def valid_move?(board, index)
-    if !position_taken?(board, index) && (index).between?(0,8)
-      return true
-    else
-      return false
-    end
-  end
-
-  #define current player
-  def current_player(board)
-     turn_count(board) % 2 == 0 ? "X" : "O"
-  end
-
-  #turn
-  def turn(board)
-    puts "Please enter 1-9:"
-    #get the user input
-    user_input = gets.strip
-    #input to index
-    index = input_to_index(user_input)
-    #check for validation
-    if valid_move?(board, index)
-      move(board, index, current_player(board))
-      display_board(board)
-    else
-      turn(board)
-    end
-  end
-
-  #counts occupied positions
-  def turn_count(board)
-     counter = 0
-     board.each {|spaces|
-        if spaces == "X" || spaces == "O"
-           counter += 1
-        end
-     }
-     counter
-  end
-
-  #winning combo
-  def won?(board)
-      WIN_COMBINATIONS.each do |win_combination|
-        win_index_1 = win_combination[0]
-        win_index_2 = win_combination[1]
-        win_index_3 = win_combination[2]
-        position_1 = board[win_index_1] # value of board at win_index_1
-        position_2 = board[win_index_2] # value of board at win_index_2
-        position_3 = board[win_index_3] # value of board at win_index_3
-        if position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
-          return win_combination
-        end
+  def turn_count
+    taken = 0
+    @board.each do |i|
+      if i == "X" || i == "O"
+        taken += 1
       end
-      return false
+    end
+    return taken
   end
-  #position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
-  #The above code means to return first element (position_1) & make sure the position is taken by X or O
 
-  def full?(board)
-    if board.any? {|index| index == nil || index == " "}
-      return false
+  def current_player
+    player = nil
+    if turn_count() % 2 == 0
+      player = 'X'
     else
-      return true
+      player = 'O'
+    end
+    return player
+  end
+
+
+  def won?
+    WIN_COMBINATIONS.detect do |combo|
+      @board[combo[0]] == @board[combo[1]] &&
+      @board[combo[1]] == @board[combo[2]] &&
+      position_taken?(combo[0])
     end
   end
 
-  def draw?(board)
-     if !won?(board) && full?(board)
-       return true
-     elsif!full?(board) && !won?(board)
-       return false
-     else won?(board)
-       return false
-     end
+  def full?
+    turn_count == 9
   end
 
-  def over?(board)
-    if draw?(board) || won?(board) || full?(board)
-      return true
-    else
-      return false
+  def draw?
+    !won? && full?
+  end
+
+  def over?
+    won? || full? || draw?
+  end
+
+  def winner
+    won = ""
+    if winner = won?
+      won = @board[winner.first]
     end
   end
 
-  #determine the winner
-  def winner(board)
-     if won?(board)
-        return board[won?(board)[0]]
-     end
-  end
-
-  #asks for players input on a turn of the game
-  def play(board)
-     counter = 0
-     until counter == 9
-       turn(board)
-       counter += 1
-     end
-  end
-
-  def play(board)
-    until over?(board)
-      turn(board)
+  def play
+    until over?
+      turn
     end
-    if won?(board)
-      winner(board) == "X" || winner(board) == "O"
-      puts "Congratulations #{winner(board)}!"
-    else draw?(board)
-      puts "Cat\'s Game!"
+
+    if won?
+      winner = winner()
+      puts "Congratulations #{winner}!"
+    elsif draw?
+      puts "Cat's Game!"
     end
   end
 end
+
+
+
+
